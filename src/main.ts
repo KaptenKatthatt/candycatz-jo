8b65ef231a1cf832e9851ed3dba09f68a9fea164
Jonas Olson <jonas.olson@gmail.com>
2025-11-25 15:51:01 +0100

    Made an example bootstrap card with api call info.

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,12 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "./assets/scss/app.scss";
 import { getCandyProductInfo, getAllCandyInfo } from "./services/candyAPI";
+import { UI } from "./ui";
+
+let candyproduct = await getCandyProductInfo();
+console.log("Candyproduct", candyproduct);
+
+console.log(candyproduct.data.images.thumbnail);
+UI.exampleTitleEl.innerText = candyproduct.data.name;
+UI.exampleCardTextEl.innerHTML = candyproduct.data.description;
+UI.exampleThumbnailEl.src = `https://www.bortakvall.se${candyproduct.data.images.thumbnail}`;

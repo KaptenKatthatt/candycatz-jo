df436175b7ccf7662a22c5bf5ef95c8ed8a4d08b
Jonas Olson <jonas.olson@gmail.com>
2025-11-26 12:50:43 +0100

    First draft of modal code working on thumbnail click.

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import * as bootstrap from "bootstrap";
 import "./assets/scss/app.scss";
 import { getCandyProductInfo, getAllCandyInfo } from "./services/candyAPI";
 import { UI } from "./ui";
+import { renderModal } from "./modal";
 
 let candyproduct = await getCandyProductInfo();
 console.log("Candyproduct", candyproduct);
@@ -13,6 +14,22 @@ console.log(candyproduct.data.images.thumbnail);
 UI.exampleTitleEl.innerText = candyproduct.data.name;
 UI.exampleCardTextEl.innerHTML = candyproduct.data.description;
 UI.exampleThumbnailEl.src = `https://www.bortakvall.se${candyproduct.data.images.thumbnail}`;
+
+const myModal = document.getElementById("myModal");
+
+UI.exampleThumbnailEl.addEventListener("click", () => {
+  const exampleModal = document.getElementById("exampleModal");
+  const modal = new bootstrap.Modal(exampleModal);
+  console.log("Click");
+  modal.show();
+  renderModal(6545);
+});
+
+// const myInput = document.getElementById("myInput");
+
+// myModal.addEventListener("shown.bs.modal", () => {
+//   myInput.focus();
+// });
 
 //----ADD TO CARTtest
 let candyId: number = candyproduct.data.id;

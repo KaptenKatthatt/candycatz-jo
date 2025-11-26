31bf3d2d4728b58c9880e5929827068aba68ea2c
Sophia Englund <englundsophia@gmail.com>
2025-11-26 10:19:09 +0100

    added some sass and test addToCart()

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,30 @@ console.log(candyproduct.data.images.thumbnail);
 UI.exampleTitleEl.innerText = candyproduct.data.name;
 UI.exampleCardTextEl.innerHTML = candyproduct.data.description;
 UI.exampleThumbnailEl.src = `https://www.bortakvall.se${candyproduct.data.images.thumbnail}`;
+
+
+
+//----ADD TO CARTtest
+let candyId: number = candyproduct.data.id;
+console.log(candyId);
+interface cartItem {
+    id: number;
+    amount: number;
+}
+let candyCart: cartItem[] = [];
+
+function addToCart(candyId: number) {
+//finns produkten i redan? så ska deta addas på i amount i arrayen
+const candyIsThere = candyCart.find(item => item.id === candyId);
+	if (candyIsThere) {
+	candyIsThere.amount++
+//annars lägg till en ny object
+	} else {
+	candyCart.push({
+		id: candyId,
+		amount: 1
+	});
+	}
+	console.log("Candycart", candyCart);
+}
+addToCart(candyId);

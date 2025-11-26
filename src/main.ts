1fb76787609d8a48ee3e0294db9eb7b7fbfbb84c
Jonas Olson <jonas.olson@gmail.com>
2025-11-26 10:46:10 +0100

    Added Bootstrap JS import.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
+//Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.css";
+import * as bootstrap from "bootstrap";
+
 import "./assets/scss/app.scss";
 import { getCandyProductInfo, getAllCandyInfo } from "./services/candyAPI";
 import { UI } from "./ui";
@@ -11,29 +14,27 @@ UI.exampleTitleEl.innerText = candyproduct.data.name;
 UI.exampleCardTextEl.innerHTML = candyproduct.data.description;
 UI.exampleThumbnailEl.src = `https://www.bortakvall.se${candyproduct.data.images.thumbnail}`;
 
-
-
 //----ADD TO CARTtest
 let candyId: number = candyproduct.data.id;
 console.log(candyId);
 interface cartItem {
-    id: number;
-    amount: number;
+  id: number;
+  amount: number;
 }
 let candyCart: cartItem[] = [];
 
 function addToCart(candyId: number) {
-//finns produkten i redan? så ska deta addas på i amount i arrayen
-const candyIsThere = candyCart.find(item => item.id === candyId);
-	if (candyIsThere) {
-	candyIsThere.amount++
-//annars lägg till en ny object
-	} else {
-	candyCart.push({
-		id: candyId,
-		amount: 1
-	});
-	}
-	console.log("Candycart", candyCart);
+  //finns produkten i redan? så ska deta addas på i amount i arrayen
+  const candyIsThere = candyCart.find((item) => item.id === candyId);
+  if (candyIsThere) {
+    candyIsThere.amount++;
+    //annars lägg till en ny object
+  } else {
+    candyCart.push({
+      id: candyId,
+      amount: 1,
+    });
+  }
+  console.log("Candycart", candyCart);
 }
 addToCart(candyId);

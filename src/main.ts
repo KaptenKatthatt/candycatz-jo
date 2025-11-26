7d7c8ff157cf32a2d18e1ac8ef0d2c42bf112403
Sophia Englund <englundsophia@gmail.com>
2025-11-26 14:28:51 +0100

    fixed merge :')

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,7 @@ import "./assets/scss/app.scss";
 import { getCandyProductInfo, getAllCandyInfo } from "./services/candyAPI";
 import { UI } from "./ui";
 import { renderModal } from "./modal";
-import { theCartView } from "./accordian";
-import { theCheckoutForm } from "./accordian";
+import { theCartView, theCheckoutForm } from "./accordian";
 
 let candyproduct = await getCandyProductInfo();
 console.log("Candyproduct", candyproduct);

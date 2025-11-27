170be091cfe757dc3cecc494e4fa02077dbf0a61
Klara Thell Lenntorp <klara.thelllenntorp@gmail.com>
2025-11-27 14:50:46 +0100

    Added + - element

--- src/components/offcan.ts
+++ src/components/offcan.ts
@@ -1,15 +1,29 @@
-const offCan = document.querySelector<HTMLDivElement>("#offCan")!;
+export const offCan = document.querySelector<HTMLDivElement>("#offCan")!;
 
-export const renderOffCan = function () {
-  offCan.innerHTML = `
+
+
+offCan.innerHTML = `
 <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
   <div class="offcanvas-header">
-    <h5 class="offcanvas-title" id="offcanvasRightLabel">Your CandyCart </h5><i class="bi bi-cart4 fs-3"></i>
+    <h5 class="offcanvas-title" id="offcanvasRightLabel">Your Candy Cart🍬 </h5><i class="bi bi-cart4 fs-3"></i>
     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
   </div>
   <div class="offcanvas-body">
     Cart details here
   </div>
+
+
+
+<div class="candyCart-btn-container">
+<div class="qty-controls"> 
+
+  <button class = "minusBtn" data-action="minus" type = "button">-</button>
+
+  <input class = "product-qty" type="number" name= "product-qty" min="0" max="10" value="1"> 
+
+  <button class = "plusBtn" data-action= "plus" type = "button">+</button>
+
+
+</div>
 </div>
 `;
-};

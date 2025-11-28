ab2a173d1fcb9811f5e8c8b21131d90d77dc2869
Jonas Olson <jonas.olson@gmail.com>
2025-11-28 18:24:16 +0100

    Removed a fnutt that wasn't quite right.

--- src/components/modal.ts
+++ src/components/modal.ts
@@ -36,7 +36,7 @@ export const renderModal = async function (productId: number) {
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title modalTitle fs-5"">${candyTitle}</h5>
+          <h5 class="modal-title modalTitle fs-5">${candyTitle}</h5>
           <button type=" button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">

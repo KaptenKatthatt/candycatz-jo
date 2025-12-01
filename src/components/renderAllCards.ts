e7e0c6a6dbe90ed3a9ab54008a2575a669982c29
Sophia Englund <englundsophia@gmail.com>
2025-12-01 15:04:37 +0100

    new array with 12 toptreats

--- src/components/renderAllCards.ts
+++ src/components/renderAllCards.ts
@@ -2,15 +2,15 @@ import { getAllCandyInfo } from "../services/candyAPI";
 import type { CandyData } from "../services/candyApiTypes";
 
 // funktion för att återanvända kort strukturen flera gånger på olika kategorier
-  function cardStructure(product: CandyData): string {
-    let thumbnailURL = `https://www.bortakvall.se${product.images.thumbnail}`;
+function cardStructure(product: CandyData): string {
+  let thumbnailURL = `https://www.bortakvall.se${product.images.thumbnail}`;
 
-    return `<div class="card rounded-4 p-1" data-product-id="${
-      product.id
-    }" style="width: 10rem;">
+  return `<div class="card rounded-4 p-1" data-product-id="${
+    product.id
+  }" style="width: 10rem;">
       <img src="${thumbnailURL}" class="card-img-top click rounded-4" alt="Image of ${
-      product.name
-    }">
+    product.name
+  }">
       <div class="card-body">
       <div class="infoContainer">
         <h5 class="card-title click fs-5">${product.name}</h5>
@@ -28,7 +28,7 @@ import type { CandyData } from "../services/candyApiTypes";
       </div>
         </div>
       `;
-  }
+}
 
 export const renderAllCards = async function () {
   //Get all products from API
@@ -49,31 +49,34 @@ export const renderAllCards = async function () {
   const sliceOutTopTreats = filterTopTreats.slice(0, 12);
   console.log("is it sliced?", sliceOutTopTreats);
   // slicea sedan ut de första 12
-    const topTreatsCardsContainerEl = document.querySelector(
+  const topTreatsCardsContainerEl = document.querySelector(
     ".topTreatsCardsContainer"
   ) as HTMLDivElement;
   topTreatsCardsContainerEl.innerHTML += sliceOutTopTreats
-    .map(product => cardStructure(product)).join("");
+    .map((product) => cardStructure(product))
+    .join("");
 
-// SWEETSAVINGS Kategorien
-const sweetSavingsCategories = [...allCandyCards];
-// ytlig kopia av allCandyCards
-const filterSweetSaving = sweetSavingsCategories.filter((candy) => {
-  return candy.stock_quantity > 8 && candy.stock_status === "instock";
-});
-// returnera ny array med alla som är instock OCH färre än 3
-const sliceOutSavings = filterSweetSaving.slice(0, 12);
-// slicea sedan ut de första 12
-console.log("is it sweet savingsliced?", sliceOutSavings);
-const sweetSavingsCardsContainterEl = document.querySelector(
+  // SWEETSAVINGS Kategorien
+  const sweetSavingsCategories = [...allCandyCards];
+  // ytlig kopia av allCandyCards
+  const filterSweetSaving = sweetSavingsCategories.filter((candy) => {
+    return candy.stock_quantity > 8 && candy.stock_status === "instock";
+  });
+  // returnera ny array med alla som är instock OCH färre än 3
+  const sliceOutSavings = filterSweetSaving.slice(0, 12);
+  // slicea sedan ut de första 12
+  console.log("is it sweet savingsliced?", sliceOutSavings);
+  const sweetSavingsCardsContainterEl = document.querySelector(
     ".sweetSavingsCardsContainer"
   ) as HTMLDivElement;
   sweetSavingsCardsContainterEl.innerHTML += sliceOutSavings
-  .map(product => cardStructure(product)).join("");
+    .map((product) => cardStructure(product))
+    .join("");
 
-// skapa variabel som innehåller de som redan har visats?
-const usedIds = [...sliceOutTopTreats.map(candy => candy.id),
-  ...sliceOutSavings.map(candy => candy.id)
-];
-console.log("använda id:", usedIds)
-}
+  // skapa variabel som innehåller de som redan har visats?
+  const usedIds = [
+    ...sliceOutTopTreats.map((candy) => candy.id),
+    ...sliceOutSavings.map((candy) => candy.id),
+  ];
+  console.log("använda id:", usedIds);
+};

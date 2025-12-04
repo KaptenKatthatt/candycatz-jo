dcf21c0fb5daf6ec5fedec63b3184f9c421efd81
Jonas Olson <jonas.olson@gmail.com>
2025-12-04 10:25:06 +0100

    Refactor cart and offcanvas components for improved structure and functionality. Added styling on injected cart product item.

--- src/components/cart.ts
+++ src/components/cart.ts
@@ -1,16 +1,12 @@
 import { getCandyProductInfo } from "../services/candyAPI";
-// import { openOffCanvas } from "./offcan";
-
-// import { type CandyData, type CandyResponse } from "../services/candyApiTypes";
+import { openOffCanvas } from "./offcan";
 
 const allCardsContainerEl =
   document.querySelector<HTMLDivElement>(".allCardsContainer");
-// const cartAmountEl =
-//   document.querySelector<HTMLParagraphElement>(".cartAmount");
-// const cartContentsEl =
-//   document.querySelector<HTMLParagraphElement>(".cartContents");
-// const cartTotalPriceEl =
-//   document.querySelector<HTMLParagraphElement>(".totalPrice");
+const cartAmountEl =
+  document.querySelector<HTMLParagraphElement>(".cartAmount");
+const cartTotalPriceEl =
+  document.querySelector<HTMLParagraphElement>(".totalPrice");
 
 let clickedCandyId = 0;
 let cartArray: CartProduct[] = [];
@@ -41,6 +37,7 @@ class CartProduct {
   }
 }
 
+//Spreada CandyResponse till en ny array och lägg på egenskapen amount på den.
 export const increaseAmountOfProductInCart = function (clickedCandyId: number) {
   const candyFound = cartArray.find(
     (product: CartProduct) => product.id === clickedCandyId
@@ -59,14 +56,14 @@ export const decreaseAmountOfProductInCart = function (clickedCandyId: number) {
     candyFound.amount--;
   }
   console.log("CartArray after amount--", cartArray);
-  // if (cartAmountEl) {
-  //   cartAmountEl.innerText = `Nbr of  products in cart ${String(
-  //     getTotalAmountOfProductsInCart()
-  //   )}`;
-  //   cartTotalPriceEl!.innerText = `Total cost: ${String(
-  //     getTotalCostOfProductsInCart()
-  //   )}`;
-  // }
+  if (cartAmountEl) {
+    cartAmountEl.innerText = `Nbr of  products in cart ${String(
+      getTotalAmountOfProductsInCart()
+    )}`;
+    cartTotalPriceEl!.innerText = `Total cost: ${String(
+      getTotalCostOfProductsInCart()
+    )}`;
+  }
 };
 
 export const deleteProductFromCart = function (clickedCandyId: number) {
@@ -86,7 +83,7 @@ export const getTotalCostOfProductsInCart = function () {
   return cartArray.reduce((acc, curr) => acc + curr.totalCost, 0);
 };
 
-const renderCart = function () {
+export const renderCart = function () {
   const cartContainerEl =
     document.querySelector<HTMLDivElement>(".cartContainer");
   // Render a card with added item
@@ -95,15 +92,20 @@ const renderCart = function () {
       .map((product) => {
         let thumbnailURL = `https://www.bortakvall.se${product.thumbnail}`;
         return `
-        <div class="container-fluid d-flex flex-row rounded-4 p-1" data-product-id="${product.id}" >
+        <div class="productItem container-fluid d-flex flex-row rounded-4 p-1" data-product-id="${product.id}" >
             <img src="${thumbnailURL}" width="50" class="img-fluid rounded-4 me-2" alt="Image of ${product.name}">
-            <h5 class="card-title click fs-5 me-2">${product.name}</h5>
-            <p class="me-2">Scoop price: ${product.price}</p>
-            <p class="me-2">Total product price: ${product.totalCost}</p>
+            <div class="container div-flex flex-column"
+              <h5 class="card-title click fs-5 me-2">${product.name}</h5>
+              <p class="me-2">${product.price}/skopa</p>
+              <p class="me-2">Produktpris: ${product.totalCost}</p>
+            </div>
+            <div class="buttonContainer d-flex flex-row align-items-center">
             <button class="increaseBtn plusBtn" type="button">+</button>
             <p class="me-2">${product.amount}</p>
             <button class="decreaseBtn minusBtn me-2"  type="button">-</button>
             <button class="deleteBtn btn btn-sm btn-danger"><i class="bi bi-trash"></i></button>
+            </div>
+
         </div>
             <hr>
 
@@ -113,15 +115,13 @@ const renderCart = function () {
 
     cartContainerEl.onclick = (e) => {
       const target = e.target as HTMLElement;
-      const candyCard = target.closest<HTMLDivElement>(".card");
+      const candyCard = target.closest<HTMLDivElement>(".productItem");
       clickedCandyId = Number(candyCard?.dataset.productId);
 
       if (target.closest(".increaseBtn")) {
         increaseAmountOfProductInCart(clickedCandyId);
-        // renderCart();
       } else if (target.closest(".decreaseBtn")) {
         decreaseAmountOfProductInCart(clickedCandyId);
-        // renderCart();
       } else if (target.closest(".deleteBtn")) {
         deleteProductFromCart(clickedCandyId);
       }
@@ -143,7 +143,7 @@ const renderCart = function () {
   );
 };
 
-const renderCartBadge = function () {
+export const renderCartBadge = function () {
   const navCartBadgeEl =
     document.querySelector<HTMLSpanElement>(".navCartBadge");
   if (navCartBadgeEl) {
@@ -161,7 +161,7 @@ allCardsContainerEl?.addEventListener("click", async (e) => {
     clickedCandyId = Number(candyCard?.dataset.productId);
     await addToCart(clickedCandyId);
     renderCartBadge();
-    // openOffCanvas();
+    openOffCanvas();
   }
 });
 

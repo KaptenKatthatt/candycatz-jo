eb4ad4c95486c0dda34a0d0fc2bac923de10b7ae
Jonas Olson <jonas.olson@gmail.com>
2025-12-07 13:49:46 +0100

    Added side scroll on load fix and fixed typo in sidescroller

--- src/main.ts
+++ src/main.ts
@@ -43,6 +43,10 @@ window.addEventListener(
   },
   { once: true }
 );
+// if ("scrollRestoration" in history) {
+//   history.scrollRestoration = "manual";
+// }
+// window.scrollTo(0, 0);
 
 renderCartView();
 renderCheckoutForm();

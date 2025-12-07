29e646a78c3cab607694463f32761fe80c6cd701
Jonas Olson <jonas.olson@gmail.com>
2025-12-07 18:44:34 +0100

    Brightness, fonts, img margin, removed dots

--- src/main.ts
+++ src/main.ts
@@ -21,18 +21,24 @@ import { sideScroller } from "./components/sideScroller";
 
 //SCSS imports
 import "./assets/scss/app.scss";
+<<<<<<< HEAD
 import "./assets/scss/cards.scss";
+=======
+>>>>>>> 3de1c28 (Brightness, fonts, img margin, removed dots)
 import "./assets/scss/navbar.scss";
 import "./assets/scss/modal.scss";
 import "./assets/scss/offcan.scss";
 import "./assets/scss/renderAllCards.scss";
 import "./assets/scss/sideScroller.scss";
 import "./assets/scss/typography.scss";
+<<<<<<< HEAD
 
 // if ("scrollRestoration" in history) {
 //   history.scrollRestoration = "manual";
 // }
 window.scrollTo(0, 0);
+=======
+>>>>>>> 3de1c28 (Brightness, fonts, img margin, removed dots)
 
 import { sendOrder } from "./components/ApiTest";
 // Prevents scroll on page load

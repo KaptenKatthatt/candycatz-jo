12aa56e6371fa940bf0f6c4bc8e01027d7c731e0
Jonas Olson <jonas.olson@gmail.com>
2025-12-09 10:08:24 +0100

    Refactoring

--- src/components/cardStructure.ts
+++ src/components/cardStructure.ts
@@ -3,8 +3,8 @@ import type { CandyData } from "../services/candyApiTypes";
 export const cardStructure = function (product: CandyData): string {
   let thumbnailURL = `https://www.bortakvall.se${product.images.thumbnail}`;
 
-  return `<div class="card cardTrans rounded-4 p-1 ${
-    product.stock_status !== "instock" ? "cardDis" : ""
+  return `<div class="card cardTransform rounded-4 p-1 ${
+    product.stock_status !== "instock" ? "cardDisabled" : ""
   }" " data-product-id="${product.id}" style="width: 11rem;">
       <img src="${thumbnailURL}" class="card-img-top click rounded-4" alt="Image of ${
     product.name

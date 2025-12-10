73afc646a762cb9bcf415dfaef16696c7e2b1100
Jonas Olson <jonas.olson@gmail.com>
2025-12-10 11:41:37 +0100

    Removed export

--- src/services/candyPOST_API.ts
+++ src/services/candyPOST_API.ts
@@ -53,7 +53,7 @@ export const sendOrder = async function (newOrderData: CheckoutData) {
       return;
     }
 
-    export const responseData = await response.json();
+    const responseData = await response.json();
 
     if (responseData.status !== "success") {
       console.log("Problems with the sent order", responseData.status);

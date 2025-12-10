5d6fcd56ed83cf380dca763bc7aff6cd8e877272
Sophia Englund <englundsophia@gmail.com>
2025-12-10 21:40:07 +0100

    function to open navbavlinks to main from checkout

--- src/components/accordian.ts
+++ src/components/accordian.ts
@@ -250,3 +250,28 @@ export const postUserAddressForm = function (responseData: ResponseData) {
       `;
       // clearCart();
 };
+
+const navBarEl = document.querySelector<HTMLLinkElement>(".navbar")!;
+const accordianEl = document.querySelector<HTMLDivElement>("#cartCheckoutContainer")!;
+const allCardsContainerEl = document.querySelector<HTMLDivElement>(".allCardsContainer")!;
+const carouselContainerEl = document.querySelector<HTMLDivElement>(".carouselContainer")!;
+const scrollcontainerEl = document.querySelector<HTMLDivElement>(".scroll-container")!;
+
+navBarEl.addEventListener("click", (e) =>{
+  const target = e.target as HTMLDivElement;
+  const clickedNavLink = target.closest(".nav-link");
+  if (clickedNavLink){
+    console.log("klickade på en nav-link", clickedNavLink)
+      console.log('har data-bs-toggle:', clickedNavLink.hasAttribute('data-bs-toggle'));
+    if (!clickedNavLink.hasAttribute("data-bs-toggle")) {
+    console.log("klickade på en cart")
+    } 
+    if (accordianEl) {
+      accordianEl.classList.add("d-none");
+      allCardsContainerEl.classList.remove("d-none");
+      carouselContainerEl.classList.remove("d-none");
+      scrollcontainerEl.classList.remove("d-none");
+    }
+
+}
+});

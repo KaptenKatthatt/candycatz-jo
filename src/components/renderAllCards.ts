68644d1c65327a9ce07bcd89680c529b3f65891d
Sophia Englund <englundsophia@gmail.com>
2025-12-10 15:18:06 +0100

    new structure on render all cards

--- src/components/renderAllCards.ts
+++ src/components/renderAllCards.ts
@@ -1,5 +1,7 @@
 import { getAllCandyInfo } from "../services/candyAPI";
 import type { CandyData } from "../services/candyApiTypes";
+import { cardStructure } from "./cardStructure";
+import { cardStructureSideScroller } from "./sideScroller";
 
 let showMoreCandy: CandyData[];
 let addedCandyNr: number = 12;
@@ -9,60 +11,42 @@ let showRestCandy: CandyData[];
 let response = await getAllCandyInfo();
 let allCandyCards: CandyData[] = response.data;
 
-// funktion för att återanvända kort strukturen flera gånger på olika kategorier
-export const cardStructure = function (product: CandyData): string {
-  let thumbnailURL = `https://www.bortakvall.se${product.images.thumbnail}`;
-
-  return `<div class="card cardTransform rounded-4 p-1 ${
-    product.stock_status !== "instock" ? "cardDisabled" : ""
-  }" " data-product-id="${product.id}" style="width: 11rem;">
-      <img src="${thumbnailURL}" class="card-img-top click rounded-4" alt="Image of ${
-    product.name
-  }">
-      <div class="card-body">
-      <div class="infoContainer">
-        <h6 class="card-title click fs-5">${product.name}</h6>
-        ${
-          product.stock_status === "instock"
-            ? `<p class="card-text stockStatus">I lager:
-            <span class="fw-bold">${product.stock_quantity}</span>`
-            : `<p class="card-text stockStatus"><em>Ej i lager</em>`
-        }</p>
-        <p class="card-text priceTag">Pris/skopa: <span class="fw-bold">${
-          product.price
-        }:-</span></p>
-        <button class="modalInfoBtn btn btn-primary my-2"><i class="bi bi-info-circle"></i></button>
-        <button class="addToCartBtn btn btn-success" 
-        ${
-          product.stock_status !== "instock" ? "disabled" : ""
-        }>+<i class="bi bi-basket ps-2"></i></button>
-        </div>
-      </div>
-        </div>
-      `;
-};
 
 export const renderAllCards = async function () {
-  //Get all products from API
-  // const allCandyCards: CandyData[] = await getAllCandyInfo();
 
-  //TODO Add if check if response.status === "success"
+    // TOP TREATS Kategorien
+  const topTreatsCategories = [...allCandyCards];
+  // ytlig kopia av allCandyCards
+  const filterTopTreats = topTreatsCategories.filter((candy) => {
+    return candy.stock_quantity < 3 && candy.stock_status === "instock";
+  });
+  // returnera ny array med alla som är instock OCH färre än 3
+  const sliceOutTopTreats = filterTopTreats.slice(0, 12);
+  // slicea sedan ut de första 12
+  const topTreatsCardsContainerEl = document.querySelector(
+    ".topTreatsSideScrollerContainer"
+  ) as HTMLDivElement;
 
-  // //TOP TREATS Kategorien
-  // const topTreatsCategories = [...allCandyCards];
-  // // ytlig kopia av allCandyCards
-  // const filterTopTreats = topTreatsCategories.filter((candy) => {
-  //   return candy.stock_quantity < 3 && candy.stock_status === "instock";
-  // });
-  // // returnera ny array med alla som är instock OCH färre än 3
-  // const sliceOutTopTreats = filterTopTreats.slice(0, 12);
-  // // slicea sedan ut de första 12
-  // const topTreatsCardsContainerEl = document.querySelector(
-  //   ".topTreatsCardsContainer"
-  // ) as HTMLDivElement;
-  // topTreatsCardsContainerEl.innerHTML += sliceOutTopTreats
-  //   .map((product) => cardStructure(product))
-  //   .join("");
+  topTreatsCardsContainerEl.innerHTML = sliceOutTopTreats
+    .map((product) => cardStructureSideScroller(product))
+    .join("");
+
+  // Side scroller arrow functionality
+  document
+    .querySelector(".topTreatsSideScrollerWrapper")
+    ?.addEventListener("click", (e) => {
+      const target = e.target as HTMLElement;
+      const scrollContainer = document.querySelector(
+        ".topTreatsSideScrollerContainer"
+      ) as HTMLDivElement;
+
+      if (target.closest(".scrollArrowLeft")) {
+        scrollContainer.scrollBy(-800, 0);
+      }
+      if (target.closest(".scrollArrowRight")) {
+        scrollContainer.scrollBy(800, 0);
+      }
+    });
 
   // SWEETSAVINGS Kategorien
   const sweetSavingsCategories = [...allCandyCards];
@@ -85,6 +69,7 @@ export const renderAllCards = async function () {
   usedIds = [
     // ...sliceOutTopTreats.map((candy) => candy.id),
     ...sliceOutSavings.map((candy) => candy.id),
+    ...sliceOutTopTreats.map((candy) => candy.id)
   ];
 
   showMoreCandy = allCandyCards.filter((candy) => !usedIds.includes(candy.id));
@@ -101,7 +86,6 @@ export const renderAllCards = async function () {
   // plussa på 12 att lägga ut "ovanpå" de previous 12, 24 + osv
   showNumberOfCandys();
 };
-
 function loadMoreSweets() {
   const moreToMunchCardsContainerEl = document.querySelector(
     ".moreToMunchCardsContainer"

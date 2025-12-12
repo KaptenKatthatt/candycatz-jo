75e20b7a0ab5e6c5ba4e3fc8679bfe0d7038599d
Sophia Englund <englundsophia@gmail.com>
2025-12-12 09:37:47 +0100

    fixed modal

--- src/components/modal.ts
+++ src/components/modal.ts
@@ -21,13 +21,16 @@ export const renderModal = async function (productId: number) {
 <div class="modal fade" id="candyModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-sm-sm">
       <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+        </button>
+        </div>
         <div class="modal-body">
-        <div class="row">
-        
+        <div class="row">    
         <div class="col-sm-6 d-flex align-items-center">
           <img src="${modalThumbnailURL}" class="modalThumbnail card-img" alt="${candyTitle}">
           </div>
-          <div class="col-6">
+          <div class="col-sm-6">
           <div class="scrollableModal">
           <p class="modalParagraph card-text pt-2">${candyParagraph}</p>
           <p class="modal-price">Pris:<strong> ${candyPrice}:- </strong></p>

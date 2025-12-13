accdaf65dd6ddae3889c8e0f11612dcf6f81c9c8
Sophia Englund <englundsophia@gmail.com>
2025-12-13 12:51:25 +0100

    small fix in modal

--- src/components/modal.ts
+++ src/components/modal.ts
@@ -22,24 +22,24 @@ export const renderModal = async function (productId: number) {
     <div class="modal-dialog modal-sm-sm">
       <div class="modal-content">
       <div class="modal-header">
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+      <h3 class="modal-title">${candyTitle}</h3>
+        <button type="button" class="btn-close btn" data-bs-dismiss="modal" aria-label="Close">
         </button>
         </div>
-        <div class="modal-body">
+        <div class="modal-body mt-0">
         <div class="row">    
         <div class="col-sm-6 d-flex align-items-center">
-          <img src="${modalThumbnailURL}" class="modalThumbnail card-img" alt="${candyTitle}">
+          <img src="${modalThumbnailURL}" class="modalThumbnail card-img scallopbox" alt="${candyTitle}">
           </div>
-          <div class="col-sm-6">
+          <div class="col-sm-6 mb-2">
           <div class="scrollableModal">
           <p class="modalParagraph card-text pt-2">${candyParagraph}</p>
           <p class="modal-price">Pris:<strong> ${candyPrice}:- </strong></p>
           <p>I lager: ${candyProduct.data.stock_quantity}</p>
         </div>
         </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Stäng</button>
-        </div>
+          <button type="button" class="btn btn-secondary modalBtn mt-2" data-bs-dismiss="modal">Stäng</button>
+       
         </div>
       </div>
   </div>

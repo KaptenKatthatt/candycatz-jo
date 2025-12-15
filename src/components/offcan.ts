e304e67332e93eed2d9a6eda61650a741df2affc
Sophia Englund <englundsophia@gmail.com>
2025-12-15 14:13:24 +0100

    fixing small offcan stuffy

--- src/components/offcan.ts
+++ src/components/offcan.ts
@@ -7,8 +7,8 @@ export const renderOffCan = function () {
   offCan.innerHTML = `
   <div class="offcanvas offcanvas-end rounded-top-4 tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel" data-bs-scroll="false">
   <div class="offcanvas-header">
-    <h5 class="offcanvas-title" id="offcanvasRightLabel">Your Candy Cart🍬 </h5>
-    <i class="bi bi-cart4 fs-3"></i>
+    <h5 class="offcanvas-title" id="offcanvasRightLabel">Your Candy Cart </h5>
+    <i class="bi bi-cart4 fs-3 ps-2"></i>
     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
   </div>
 

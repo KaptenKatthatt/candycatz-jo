3af54ce24c0c00960a4fb1ecfe38fd5f50454b2a
Jonas Olson <jonas.olson@gmail.com>
2025-12-17 10:38:39 +0100

    Changed godisdag to jpg

--- src/components/carousel.ts
+++ src/components/carousel.ts
@@ -13,7 +13,7 @@ export const renderCarousel = function () {
           <img src="/img/suris.png" class="d-block w-100" alt="Picture with sour sweet treats" />
         </div>
         <div class="carousel-item">
-          <img src="/img/godisdag.png" class="d-block w-100" alt="Picture of soft mashmallow" />
+          <img src="/img/godisdag.jpg" class="d-block w-100" alt="Picture of soft mashmallow" />
         </div>
       </div>
       <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"

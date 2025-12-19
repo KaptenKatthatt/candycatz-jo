bbb7ca25b6b4f3b161a1c23938f30bb88969013c
Jonas Olson <jonas.olson@gmail.com>
2025-12-19 22:12:22 +0100

    Installed Styleint autolinter for SCSS

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import("stylelint").Config} */
 export default {
-  "extends": ["stylelint-config-standard"]
+  extends: ["stylelint-config-standard-scss"],
+  plugins: ["stylelint-scss"],
+  rules: {
+    // Lägg till egna regler här om nödvändigt
+  },
 };

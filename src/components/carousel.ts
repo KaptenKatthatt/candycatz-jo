2227b173714b3a1eb20eeed84ebb4321a9fa235f
Jonas Olson <jonas.olson@gmail.com>
2025-12-21 12:29:35 +0100

    Added more templates

--- src/components/carousel.ts
+++ src/components/carousel.ts
@@ -1,3 +1,5 @@
+import { carouselTemplate } from "./templates";
+
 export const carouselContainerEl = document.querySelector(
   ".carousel-container"
 );
@@ -7,28 +9,5 @@ if (!carouselContainerEl) {
 }
 
 export const renderCarousel = function () {
-  carouselContainerEl.innerHTML = `
-<div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="/img/sockerKick.png" class="d-block w-100" alt="Picture of yummy candy" fetchpriority="high" loading="eager"/>
-        </div>
-        <div class="carousel-item">
-          <img src="/img/suris.webp" class=" w-100" alt="Picture with sour sweet treats"/>
-        </div>
-        <div class="carousel-item">
-          <img src="/img/godisdag.webp" class=" w-100" alt="Picture of soft mashmallow"/>
-        </div>
-      </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
-        data-bs-slide="prev">
-        <span aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
-        data-bs-slide="next">
-        <span aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
-    </div>`;
+  carouselContainerEl.innerHTML = carouselTemplate();
 };

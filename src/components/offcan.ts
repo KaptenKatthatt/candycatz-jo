a465680f217fa3d7069ff4edf8f5e963ea05d4cd
Jonas Olson <jonas.olson@gmail.com>
2025-12-21 13:05:43 +0100

    Made more templates

--- src/components/offcan.ts
+++ src/components/offcan.ts
@@ -1,68 +1,11 @@
 import { renderCart, renderCartBadge } from "./cart";
 import { renderCheckoutCart } from "./renderCheckoutCart";
+import { offCanvasTemplate } from "./templates";
 
 export const offCan = document.querySelector<HTMLDivElement>("#offCan")!;
 
 export const renderOffCan = function () {
-  offCan.innerHTML = `
-    <div
-      class="offcanvas offcanvas-end rounded-top-4"
-      tabindex="-1"
-      id="offcanvasRight"
-      aria-labelledby="offcanvasRightLabel"
-      data-bs-scroll="false"
-    >
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasRightLabel">Your Candy Cart</h5>
-        <i class="bi bi-cart4 fs-3 ps-2"></i>
-        <button
-          type="button"
-          class="btn-close"
-          data-bs-dismiss="offcanvas"
-          aria-label="Close"
-        ></button>
-      </div>
-
-      <!-- Cart products container -->
-      <div class="offcanvas-body">
-        <div class="cart-product-container card">
-          <div class="card-body">
-            <div class="row align-items-center">
-              <!-- Inject cart contents from cart.ts -->
-              <div class="cart-container"></div>
-            </div>
-            <hr />
-            <div class="d-flex justify-content-between mb-4">
-              <strong>Totalt</strong>
-              <span class="total-cost-container"><strong></strong></span>
-            </div>
-          </div>
-        </div>
-
-        <!-- Continue Shopping Button -->
-        <button
-          type="button"
-          id="continue-shopping-btn"
-          class="btn"
-          data-bs-dismiss="offcanvas"
-          aria-label="Close"
-        >
-          <i class="bi bi-arrow-left me-2"></i>Fortsätt handla
-        </button>
-
-        <button
-          class="checkout-btn"
-          data-bs-dismiss="offcanvas"
-          aria-label="Close"
-        >
-          Gå till kassan
-          <i class="bi bi-arrow-right ms-2"></i>
-        </button>
-
-        <div class="heart-cat-container"></div>
-      </div>
-    </div>
-  `;
+  offCan.innerHTML = offCanvasTemplate();
   // EventListener for Proceed to Checkout
   const cartCheckoutContainer = document.querySelector<HTMLDivElement>(
     "#cartCheckoutContainer"
